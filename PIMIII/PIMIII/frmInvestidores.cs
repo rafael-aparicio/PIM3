0e978e9b7c3b08a8f8f41ea294c29a726811d074
rafael-aparicio <62367920+rafael-aparicio@users.noreply.github.com>
2020-11-23 03:58:28 -0400

    Alteração em telas

---
+++ PIMIII/PIMIII/frmInvestidores.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PIMIII
+{
+    public partial class frmInvestidores : Form
+    {
+        public frmInvestidores()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuFlatButton8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomTextbox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton8_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void bunifuGradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomTextbox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomTextbox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomTextbox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmInvestidores_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomTextbox12_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomTextbox13_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomTextbox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomTextbox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomTextbox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomTextbox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomTextbox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            Close();
+
+        }
+    }
+}
